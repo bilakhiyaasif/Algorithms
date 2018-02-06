cc75db04b9d42c98b8f3813a8a70182a54a825fc
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-02-07 03:27:51 +0530

    Create Solution.java

---
+++ Warmup/7-Staircase/Solution.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static void staircase(int n) {
+        // Complete this function
+		int t=(n-1);
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j>=t)
+				{
+					System.out.print("#");
+					
+				}
+				
+				
+				else
+				{
+					System.out.print(" ");
+				}
+				
+				
+				
+			}
+			t--;
+			System.out.print("\n");
+		}
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        staircase(n);
+        in.close();
+    }
+}
